e93037effa9d0ff1ad043650aaaab52a24e341b2
KIRUBEL187 <101458686+KIRUBEL187@users.noreply.github.com>
2025-08-04 22:36:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-
-let current = 0;
-const sections = document.querySelectorAll(".section");
-
-function show(index) {
-  const container = document.querySelector("main");
-  container.style.transform = `translateX(-${index * 100}vw)`;
-}
-document.body.addEventListener("wheel", (e) => {
-  if (e.deltaY > 0) current++;
-  else current--;
-  current = Math.max(0, Math.min(sections.length - 1, current));
-  show(current);
-});
 document.getElementById("toggle-theme").addEventListener("click", () => {
-  document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("light-mode");
 });
